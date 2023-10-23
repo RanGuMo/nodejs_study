48540fcf38991548a4b73637282a5a83cd3875a2
zack <1126146235@qq.com>
2023-10-23 15:44:38 +0800

    修改密码

--- 03_api_server/src/router/userinfo.js
+++ 03_api_server/src/router/userinfo.js
@@ -10,7 +10,7 @@ const userinfo_handler = require('../router_handle/userinfo_handle')
 // 1. 导入验证表单数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
-const { update_userinfo_schema } = require('../schema/user')
+const { update_userinfo_schema,update_password_schema  } = require('../schema/user')
 
 
 
@@ -21,11 +21,11 @@ const { update_userinfo_schema } = require('../schema/user')
 // 获取用户的基本信息
 router.get('/userinfo', userinfo_handler.getUserinfo)
 
-
-
 // 更新用户的基本信息
 router.post('/userinfo',expressJoi(update_userinfo_schema), userinfo_handler.updateUserinfo)
 
+// 重置密码的路由
+router.post('/updatepwd',expressJoi(update_password_schema), userinfo_handler.updatePassword)
 
 
 
--- 03_api_server/src/router_handler/userinfo.js
+++ 03_api_server/src/router_handler/userinfo.js
@@ -1,5 +1,9 @@
 // 导入数据库操作模块
 const db = require('../db/index')
+// 在头部区域导入 bcryptjs 后，
+// 即可使用 bcrypt.compareSync(提交的密码，数据库中的密码) 方法验证密码是否正确
+// compareSync() 函数的返回值为布尔值，true 表示密码正确，false 表示密码错误
+const bcrypt = require('bcryptjs')
 
 
 // 获取用户基本信息的处理函数
@@ -38,4 +42,37 @@ exports.updateUserinfo = (req, res) => {
 
 
     })
+}
+
+// 重置密码的处理函数
+exports.updatePassword = (req, res) => {
+    const sql = `select * from ev_users where id=?`
+    db.query(sql, req.body.id, (err, results) => {
+        if (err) return res.cc(err)  // 执行 SQL 语句失败
+        // 检查指定 id 的用户是否存在
+        if (results.length !== 1) return res.cc('用户不存在！')
+        // 判断提交的旧密码是否正确
+        const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].password)
+        if (!compareResult) return res.cc('旧密码不正确！')
+        // 对新密码进行 bcrypt 加密之后，更新到数据库中
+        // 定义更新用户密码的 SQL 语句
+        const sql = `update ev_users set password=? where id=?`
+
+        // 对新密码进行 bcrypt 加密处理
+        const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+        db.query(sql, [newPwd, req.body.id], (err, results) => {
+            // SQL 语句执行失败
+            if (err) return res.cc(err)
+            // SQL 语句执行成功，但是影响行数不等于 1
+            if (results.affectedRows !== 1) return res.cc('更新密码失败！')
+            // 更新密码成功
+            res.cc('更新密码成功！', 0)
+
+        })
+
+
+
+    })
+
+    res.send('ok')
 }
