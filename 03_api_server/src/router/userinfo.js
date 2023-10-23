b9ce2efd668d33aa9fa2425b0d8746c806e26428
zack <1126146235@qq.com>
2023-10-23 15:50:23 +0800

    更新用户头像

--- 03_api_server/src/router/userinfo.js
+++ 03_api_server/src/router/userinfo.js
@@ -10,7 +10,7 @@ const userinfo_handler = require('../router_handle/userinfo_handle')
 // 1. 导入验证表单数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
-const { update_userinfo_schema,update_password_schema  } = require('../schema/user')
+const { update_userinfo_schema,update_password_schema,update_avatar_schema  } = require('../schema/user')
 
 
 
@@ -27,7 +27,8 @@ router.post('/userinfo',expressJoi(update_userinfo_schema), userinfo_handler.upd
 // 重置密码的路由
 router.post('/updatepwd',expressJoi(update_password_schema), userinfo_handler.updatePassword)
 
-
+// 更新用户头像的路由
+router.post('/update/avatar', expressJoi(update_avatar_schema),userinfo_handler.updateAvatar)
 
 
 
