dd1971fd0d13280088df38343d060a800fcb1b7f
zack <1126146235@qq.com>
2023-10-23 17:41:36 +0800

    fixBug: 全局中间件，res.cc 必须放在 jwt 和注册路由模块之前！！！！！！！

--- 03_api_server/app.js
+++ 03_api_server/app.js
@@ -10,6 +10,35 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+const config = require('./src/utils/config')
+
+// 解析 token 的中间件
+const expressJWT = require('express-jwt')
+
+// 响应数据的中间件，必须放在 注册路由模块  和 jwt中间件之前！！！！！！！！！！！！！！
+app.use(function (req, res, next) {
+  // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+  res.cc = function (err, status = 1) {
+    res.send({
+      // 状态
+      status,
+      // 状态描述，判断 err 是 错误对象 还是 字符串
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next()
+})
+
+
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] }))
+
+
+
+
+
+
+
 
 // 1.导入并注册用户路由模块
 const userRouter = require('./src/router/user')
@@ -31,30 +60,6 @@ app.use('/my/article', artiCleRouter)
 
 
 
-const config = require('./src/utils/config')
-
-// 解析 token 的中间件
-const expressJWT = require('express-jwt')
-
-// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
-app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] }))
-
-// 响应数据的中间件
-app.use(function (req, res, next) {
-  // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
-  res.cc = function (err, status = 1) {
-    res.send({
-      // 状态
-      status,
-      // 状态描述，判断 err 是 错误对象 还是 字符串
-      message: err instanceof Error ? err.message : err,
-    })
-  }
-  next()
-})
-
-
-
 // 全局错误级别中间件
 app.use(function (err, req, res, next) {
   // 数据验证失败
@@ -64,7 +69,7 @@ app.use(function (err, req, res, next) {
   // if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
   if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！', 401)
   // 未知错误
-  res.cc(err)
+  // res.cc(err)
 })
 
 // write your code here...
--- 03_api_server/src/router_handler/user.js
+++ 03_api_server/src/router_handler/user.js
@@ -25,7 +25,7 @@ exports.regUser = (req, res) => {
 
     // 接收表单数据
     const userinfo = req.body
-    console.log(userinfo, 12);
+    console.log(userinfo, 1111);
     // 判断数据是否合法
     if (!userinfo.username || !userinfo.password) {
         return res.send({ status: 1, message: '用户名或密码不能为空' })
@@ -62,15 +62,9 @@ exports.regUser = (req, res) => {
 
 
 
-
     })
 
-
-    // 加密密码
-    // 插入新用户
-
-    // 返回响应
-    // res.send('注册成功')
+ 
 
 
 }
